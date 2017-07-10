c8f90530e86c598607296291f8bb43ba7aec5ce8
Agneev <agneev@kth.se>
2017-07-10 16:35:58 +0200

    Gabor Previous CM

--- Matlab/Confusion_matrix.m
+++ Matlab/Confusion_matrix.m
@@ -2,9 +2,9 @@
 % clear all; close all; clc;
 
 %% Input Data
-true_dir = '/home/scania/Scania/Agneev/True_Labels_blackened/9-Classes_9548-Images/Test/Labels/';
+true_dir = '/home/scania/Scania/Agneev/True_Labels_blackened/16-Classes_9548-Images/Test/Labels/';
 %true_dir = '/media/scania/iQMatic_2/Agneev_Digits/Datasets/9-Classes_1364-Images/Test/Labels/';
-pred_dir = '/home/scania/Scania/Agneev/Download_Models/Agneev_Model_9_9548_Preliminary-test_With-pretrain_Gabor_epoch_30.0/Segnet_Labels/';
+pred_dir = '/home/scania/Scania/Agneev/Download_Models/Previous/Agneev_Model_16_9548_Preliminary-test_With-previous_Gabor_epoch_29.0/Segnet_Labels/';
 im_true_all = [];
 im_pred_all = [];
 
@@ -15,7 +15,7 @@ order_9 = ["105 105 105";"112 128 144";"192 192 192";"139 69 19";"244 164 96";"1
 order_16 = ["105 105 105";"112 128 144";"192 192 192";"139 69 19";"244 164 96";"106 90 205";"255 255 0";"245 222 179";"176 196 222";"210 105 30";"210 180 140";"135 206 235";"0 100 0";"154 205 50";"255 255 224";"0 0 0"];
 
 %% Read multiple files from folder
-f_true = dir(strcat(true_dir,'*.bmp'));         % true_dir bmp for 6820 and 9548
+f_true = dir(strcat(true_dir,'*.bmp'));         % true_dir bmp for 6820 and 9548, else png
 for k = 1:size(f_true,1)
     file = strcat(true_dir,f_true(k).name);     % true_dir
     im_true{k} = imread(file);
@@ -37,5 +37,5 @@ RGB_true_Vector = reshape(im_true_all,[],3);
 RGB_pred_Vector = reshape(im_pred_all,[],3);
 RGB_true_Vector = join(string(RGB_true_Vector));
 RGB_pred_Vector = join(string(RGB_pred_Vector));
-[C_9_9548_Gabor] = confusionmat(RGB_true_Vector,RGB_pred_Vector, 'order', order_9);
+[C_16_9548_Gabor_Prev] = confusionmat(RGB_true_Vector,RGB_pred_Vector, 'order', order_16);
 
